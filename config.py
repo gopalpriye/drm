04770cb4adaea8bc6a92cccac84c92fa9199f49b
gopalpriye <govindkumaryadav8651@gmail.com>
2025-05-05 07:43:45 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_ID = os.environ.get("API_ID", "18116358") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "80e7597f27a57df271dfc500120d4ea9") #Replace With your api hash
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7594405866:AAF527H4zrRubxeLfS_mAfOIgu_idqG9MsM") #Replace With your bot token
